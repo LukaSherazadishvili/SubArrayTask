a17182c77f8966f675f4d5644c193c96f46f9b00
LukeSherazadishvili <l.sherazadishvili@upcast.ge>
2020-02-04 19:20:08 +0400

    Done

---
+++ Task1/MyCalculator.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class MyCalculator
+    {
+
+        public int GetBinary(int sizeoflist)
+        {
+            var value = MyPower(2, sizeoflist + 1) + 1;
+
+            return value;
+        }
+        public int MyPower(int Base, int topower)
+        {
+            var res = 1;
+            for (int i = 0; i < topower; i++)
+            {
+                res *= Base;
+            }
+            return res;
+        }
+
+        public double CalculateBlock(int[] array)
+        {
+            double answer = array.Length;
+            var x = Uniqueness(array);
+            var n = x.Values;
+            Dictionary<int, int>.ValueCollection valueColl = n;
+            foreach (var val in valueColl)
+            {
+                var m = Math.Log((double)array.Length / val, 2);
+                answer += val * m;
+            }
+            return answer;
+        }
+        public Dictionary<int, int> Uniqueness(int[] array)
+        {
+            Dictionary<int, int> uniques = new Dictionary<int, int>();
+            var totalNumber = array.Length;
+            for (int i = 0; i < totalNumber; i++)
+            {
+                if (!uniques.ContainsKey(array[i]))
+                {
+                    uniques.Add(array[i], 1);
+                }
+                else
+                {
+                    uniques[array[i]]++;
+                }
+            }
+            return uniques;
+        }
+        
+        
+        public List<List<int>> GetSubArrys(List<int> list, string BinaryValue)
+        {
+            var indexes = GetIndexes(BinaryValue);
+
+            var res = new List<List<int>>();
+
+            int start = 0;
+
+            for (int i = 0; i < indexes.Count; i++)
+            {
+                res.Add(GetFromStartToFinish(list, start, indexes[i]));
+
+                start = indexes[i];
+            }
+
+            
+            return res;
+        }
+        public List<List<int>> ContinueWith(List<List<int>> pre, List<int> list)
+        {
+            var count = new List<int>();
+
+            var res = new List<List<int>>();
+
+            for (int i = 0; i < pre.Count;i++)
+            {
+                count.Add(pre[i].Count);
+            }
+            count.Reverse();
+
+            var start = 0;
+
+            for(int i = 0; i < count.Count; i++)
+            {
+                res.Add(GetFromStartToFinish(list, start, count[i]));
+            }
+                
+
+            return res;
+        }
+        public List<int> GetIndexes(string BinaryValue)
+        {
+            var rez = new List<int>();
+            for (int i = 0; i < BinaryValue.Length-1; i++)
+            {
+                if (BinaryValue[i+1] == '1')
+                    rez.Add(i);
+            }
+            return rez;
+        }
+        public List<int> GetFromStartToFinish(List<int> list, int start, int finish)
+        {
+
+            var res = new List<int>();
+            var end = finish;
+            for (int i = start; i <end; i++)
+            {
+                var element = list[i];
+                res.Add(element);
+            }
+            return res;
+        }
+        
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCalculator = new MyCalculator();
+            var list = new List<int>()
+            {
+                6,7,7,7,12,12,12,12,0
+            };
+
+            var allSubArrayList = new List<List<List<int>>>();
+            var binaryNumnber = myCalculator.GetBinary(list.Count);
+
+            for (int i = 0; i < myCalculator.MyPower(2, list.Count)/2; i++)
+            {
+                string binaryValue = Convert.ToString(binaryNumnber, 2);
+                var tmpSubArray = myCalculator.GetSubArrys(list, binaryValue);
+                var mirrorTmpSubArray = myCalculator.ContinueWith(tmpSubArray, list);
+
+                allSubArrayList.Add(tmpSubArray);
+                allSubArrayList.Add(mirrorTmpSubArray);
+                binaryNumnber += 2;
+            }
+
+            var sums = new List<double>();
+            foreach(var arr in allSubArrayList)
+            {
+                double sum = 0;
+                for(int i = 0; i < arr.Count; i++)
+                {
+                    sum+=myCalculator.CalculateBlock(arr[i].ToArray<int>());
+                }
+                sums.Add(sum);
+            }
+
+            var result = sums.Min();
+            var indexOfLowestSum = sums.IndexOf(result);
+
+            var FinalResult = allSubArrayList[indexOfLowestSum];
+
+            var OUTPUT = string.Empty;
+            foreach(var item in FinalResult)
+            {
+                var str = item.Count.ToString();
+                OUTPUT += str+",";
+            }
+            Console.WriteLine(OUTPUT);
+
+            Console.ReadLine();
+        }
+    }
+}
+
